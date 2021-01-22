d921d1c11da9e0bf939d0de379ae413d23ef8611
Oriely <orielygaming@protonmail.com>
2021-01-22 14:51:16 +0100

    adding tests to old

--- primary.js
+++ primary.js
@@ -1,9 +1,119 @@
 
 
-let datee = new Date();
+let todos = {
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 1,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 2,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 1,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 1,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 1,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 2,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 2,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 2,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 3,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 3,
+        completed: false
+    },
+    [keyGen()]: {
+        id: genId(),
+        date_added: Date.now(),
+        date_edited: '',
+        date_finished: '',
+        title: '123123123',
+        content: 'asdoqdpoqkdpqkwdpoqwkdpqkwdpoqwd',
+        priority: 3,
+        completed: false
+    },
 
-
-let todos = {};
+    
+};
 
 let container = document.getElementById('container');
 let todoHTML_high = '';
@@ -20,33 +130,84 @@ let input_content_edit = '';
 let priority = [1, 2, 3];
 let filter = '';
 let sorting = '';
+let on_page = 'main';
 
 function updateScreen() {
+    if (on_page == "main") {
+        mainPage();
+    }
+    if (on_page == 'second') {
+        secondPage();
+    }
+}
+
+updateScreen();
+
+function todoCreateHTML(id) {
+    return `
+        <div class="todo-wrapper${(todos[id].completed === true ? ' fin' : '')}${setPriority(todos[id].priority)}" >
+            <div class="todo-title">${(mode == 'edit' && id == selectedToEdit ? '<input class="edit" type="text" onkeyup="input_title_edit = this.value" value="' + todos[id].title +'">' : '<h1>' + todos[id].title + '</h1>')}</div>
+            <div class="todo-dates">
+                <span>Added: ${stupidToReadableDate(todos[id].date_added)}</span>
+                <span>${(todos[id].date_finished != '' ? 'Completed: ' + stupidToReadableDate(todos[id].date_finished) : '')}</span>
+                <span>${(todos[id].date_edited != '' ? 'Edited: ' + todos[id].date_edited : '')}</span>
+            </div>
+            <div class="todo-content">  
+                ${(mode == 'edit' && id == selectedToEdit ? '<textarea class="edit" onkeyup="input_content_edit = this.value">'+ todos[id].content + '</textarea>' : todos[id].content)}
+            </div>
+            <div>Priority: ${(todos[id].priority === 1 ? 'High' : '') || (todos[id].priority === 2 ? 'Medium' : '') ||(todos[id].priority === 3 ? 'Low' : '')}</div>
+            <div class="todo-controls-edit">
+                ${(todos[id].completed == true ? '' : '<button '+ (id != selectedToEdit && mode == 'edit' ? 'disabled ' : '') + 'onclick="editTodo(\''+id+'\')">' + (id == selectedToEdit ? (mode == 'edit' ? 'Save' : 'Edit') : 'Edit') + '</button>')}
+                ${(todos[id].completed == false ? '<button ' + (mode == 'edit' ? 'disabled' : '') + ' onclick="completeTodo(\''+id+'\');">Complete</button>' : '')}
+    
+                <button onclick="removeTodo('${id}')">Remove</button>
+            </div>
+        </div>
+    `;
+}
+
+function stupidToReadableDate(date) {
+    return new Date(date).toLocaleDateString() + ' ' + new Date(date).toLocaleTimeString()
+
+}
+
+
+function mainPage() {
     todoHTML_high = '';
-    todoHTML_low = ''
+    todoHTML_low = '';
     todoHTML_medium = '';
+
+    
     for (let keys in todos) {
 
-        if (todos[keys].priority === 1) {
-            todoHTML_high += todoCreateHTML(keys);
-        }
-    }
-    for (let keys in todos) {
+        if (todos[keys].completed === false ) {
+            if (todos[keys].priority === 1) {
+                todoHTML_high += todoCreateHTML(keys);
+            }
+        
 
-        if (todos[keys].priority === 2) {
-            todoHTML_medium += todoCreateHTML(keys);
-        }
-    }
-    for (let keys in todos) {
+            if (todos[keys].priority === 2) {
 
-        if (todos[keys].priority === 3) {
-            todoHTML_low += todoCreateHTML(keys);
-        }
+                todoHTML_medium += todoCreateHTML(keys);
+            }
+
+        
+
+            if (todos[keys].priority === 3) {
+                todoHTML_low += todoCreateHTML(keys);
+            }
+        } 
     }
+        
 
 
     container.innerHTML = `
     <div class="wrapper">
+        <nav>
+            <div onclick="changeScreen('main')">Main</div>
+            <div onclick="changeScreen('second')">Completed todos</div>
+        </nav>
+        
         <div class="input-form">
             <div class="input-title"><input id="" type="text" onkeyup="input_title = this.value" placeholder="Some title...."></div>
             <div class="input-content"><textarea onkeyup="input_content = this.value"></textarea></div>
@@ -93,27 +254,58 @@ function updateScreen() {
     `;
 }
 
-updateScreen();
 
-function todoCreateHTML(id) {
+function secondPage() {
+    todoHTML_high = '';
+    todoHTML_low = '';
+    todoHTML_medium = '';
+    for (let keys in todos) {
 
-    console.log(id);
-    return `
-    <div class="todo-wrapper${(todos[id].completed === true ? ' fin' : '')}${setPriority(todos[id].priority)}" >
-        <div class="todo-title">${(mode == 'edit' && id == selectedToEdit ? '<input class="edit" type="text" onkeyup="input_title_edit = this.value" value="' + todos[id].title +'">' : '<h1>' + todos[id].title + '</h1>')}</div>
-        <div class="todo-dates">
-            <span>Added: ${todos[id].date_added}</span>
-            <span>${(todos[id].date_finished != '' ? 'Completed: ' + todos[id].date_finished : '')}</span>
-            <span>${(todos[id].date_edited != '' ? 'Edited: ' + todos[id].date_edited : '')}</span>
-        </div>
-        <div class="todo-content">  
-            ${(mode == 'edit' && id == selectedToEdit ? '<textarea class="edit" onkeyup="input_content_edit = this.value">'+ todos[id].content + '</textarea>' : todos[id].content)}
-        </div>
-        <div class="todo-controls-edit">
-            ${(todos[id].completed == true ? '' : '<button '+ (id != selectedToEdit && mode == 'edit' ? 'disabled ' : '') + 'onclick="editTodo(\''+id+'\')">' + (id == selectedToEdit ? (mode == 'edit' ? 'Save' : 'Edit') : 'Edit') + '</button>')}
-            ${(todos[id].completed == false ? '<button onclick="completeTodo(\''+id+'\');">Complete</button>' : '')}
-   
-            <button onclick="removeTodo('${id}')">Remove</button>
+        if (todos[keys].completed === true ) {
+            if (todos[keys].priority === 1) {
+                todoHTML_high += todoCreateHTML(keys);
+            }
+    
+    
+            if (todos[keys].priority === 2) {
+                todoHTML_medium += todoCreateHTML(keys);
+            }
+ 
+            if (todos[keys].priority === 3) {
+                todoHTML_low += todoCreateHTML(keys);
+            }
+        }
+        
+    }
+
+
+    container.innerHTML = `
+    <div class="wrapper">
+        <nav>
+            <div onclick="changeScreen('main')">Main</div>
+            <div onclick="changeScreen('second')">Completed todos</div>
+        </nav>
+        <div>
+                <div class="todo-filter">
+                    <label>Filter by</label>
+                    <select name="cars" id="cars" onchange="filterTodos(this)">
+                        <option ${(filter == 0 ? 'selected' + ' ': '')}value="0">Nothing</option>
+                        <option ${(filter == 1 ? 'selected' + ' ': '')}value="1">High</option>
+                        <option ${(filter == 2 ? 'selected' + ' ': '')}value="2">Medium</option>
+                        <option ${(filter == 3 ? 'selected' + ' ': '')}value="3">Low</option>
+                    </select>
+                </div>
+                <div class="todo-sort">
+                    <label>Sort by</label>
+                    <select name="cars" id="cars" onchange="sortTodos(this)">
+                        <option ${(sorting == 1 ? 'selected' + ' ': '')}value="1">High > Low</option>
+                        <option ${(sorting == 2 ? 'selected' + ' ': '')}value="2">Low > High</option>
+                    </select>
+                </div>
+                </div>
+        <div class="todos">
+            ${listTodos(filter, sorting)}
+            
         </div>
     </div>
     `;
@@ -142,19 +334,6 @@ function listTodos(filt, sort) {
 
 }
 
-function filterTodos(a) {
-    mode = '';
-    filter = a.value;
-    sorting = ''
-    updateScreen();
-}
-
-function sortTodos(a) {
-    mode = '';
-    filter = '';
-    sorting = a.value;
-    updateScreen();
-}
 
 function setPriority(pri) {
     if (pri === priority[0]) {
@@ -169,7 +348,7 @@ function setPriority(pri) {
 }
 
 function showErrors() {
-    return '<div class="errors"><p>' + errors + '<p><span>X</span></div>';
+    return '<div class="errors"><p>' + errors + '<p><span onclick="clearError()">X</span></div>';
 }
 
 function hideErrors() {
@@ -179,110 +358,3 @@ function hideErrors() {
 
 
 
-function addTodo(a, b, c) {
-    if (a && b && c) {
-        todos[keyGen()] = {
-            id: genId(),
-            date_added: date.toUTCString(),
-            date_edited: '',
-            date_finished: '',
-            title: a,
-            content: b,
-            priority: c,
-            completed: false,
-        };
-        updateScreen();
-    } else {
-        if (!input_title || !input_content || !input_priority) {
-            errors = 'Something is missing....';
-        } else {
-            errors = '';
-            let date = new Date();
-            todos[keyGen()] = {
-                id: genId(),
-                date_added: date.toUTCString(),
-                date_edited: '',
-                date_finished: '',
-                title: input_title,
-                content: input_content,
-                priority: input_priority,
-                completed: false,
-            };
-        }
-    }
-
-
-    input_title = '';
-    input_content = '';
-    input_priority = '';
-
-    updateScreen();
-}
-
-function editTodo(id) {
-
-    console.log(id);
-
-    let date = new Date();
-
-    selectedToEdit = id;
-
-    if (input_title_edit == '' && input_content_edit == '') {
-        input_title_edit = todos[id].title;
-        input_content_edit = todos[id].content;
-    }
-
-    if (mode == 'edit' && id == selectedToEdit) {
-
-        todos[id].date_edited = date.toUTCString();
-        todos[id].title = input_title_edit;
-        todos[id].content = input_content_edit;
-
-        selectedToEdit = '';
-        input_title_edit = '';
-        input_content_edit = '';
-        mode = '';
-
-    } else {
-        mode = "edit"
-    }
-
-
-    updateScreen();
-
-}
-
-function completeTodo(id) {
-    mode = '';
-    selectedToEdit = '';
-    let date = new Date();
-    todos[id].date_finished = date.toUTCString();
-    todos[id].completed = true;
-
-    updateScreen();
-}
-
-function removeTodo(id) {
-    delete todos[id];
-    updateScreen();
-}
-
-function keyGen() {
-    return '_' + Math.random().toString(36).substr(2, 9);
-}
-
-function checkId(a) {
-    if (Object.keys(todos).length < 0) {
-        for (keys in todos) {
-            return (todos[keys].id === a)
-        }
-    } else return false;
-
-}
-
-function genId() {
-    let new_id = Math.floor(Math.random() * Math.floor(Math.random() * Date.now()));
-    return (checkId(new_id) === false ? new_id : Math.floor(Math.random() * Math.floor(Math.random() * Date.now())) && console.log('winner'));
-}
-
-  
